3702bb89eea97feff874e361a8619e3fc3a3b034
sumedhtare <sumedh.tare@gmail.com>
2022-03-27 17:05:46 +0530

    quick fix

--- src/components/common/userorpost/userorpost.tsx
+++ src/components/common/userorpost/userorpost.tsx
@@ -7,7 +7,7 @@ const UserOrPost = ({ post }: any) => {
     return <>
         <p className={style.title}>{post.metadata.content}</p>
         <p className={style.description}>{post.metadata.description}</p>
-        {post.metadata.media.length > 0 && post.metadata.media.map((item: any, index: any) => {
+        {post.metadata.media?.length > 0 && post.metadata.media?.map((item: any, index: any) => {
             return <Media item={item} key={index} />
         })}
 
